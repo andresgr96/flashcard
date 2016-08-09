6c3b240bf34231ad0b4e5d602deeaec5eca50f7f
Andres Garcia and jake blank <andres@>
2016-08-09 10:36:43 -0400

    Start flashcard project

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+
+
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $(".initial-showingjs").toggle();
+    $(".initial-hiddenjs").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+    $(".initial-showingop").toggle();
+    $(".initial-hiddenop").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable3").click(function() {
+    $(".initial-showingvar").toggle();
+    $(".initial-hiddenvar").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable4").click(function() {
+    $(".initial-showingfunc").toggle();
+    $(".initial-hiddenfunc").toggle();
+  });
+});
