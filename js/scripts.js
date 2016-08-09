2a98c6f05f91e071631055ac47dea8c2b306dc3d
Andres Garcia and jake blank <jake@>
2016-08-09 13:19:47 -0400

    Add flashcard functions to code.

--- js/scripts.js
+++ js/scripts.js
@@ -27,3 +27,92 @@ $(document).ready(function() {
     $(".initial-hiddenfunc").toggle();
   });
 });
+$(document).ready(function() {
+  $(".clickable5").click(function() {
+    $(".initial-showingme").toggle();
+    $(".initial-hiddenme").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".clickable6").click(function() {
+    $(".initial-showingar").toggle();
+    $(".initial-hiddenar").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".clickable7").click(function() {
+    $(".initial-showingpar").toggle();
+    $(".initial-hiddenpar").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".clickable8").click(function() {
+    $(".initial-showingre").toggle();
+    $(".initial-hiddenre").toggle();
+  });
+});
+
+
+$(document).ready(function() {
+  $(".clickable9").click(function() {
+    $(".initial-showingcm").toggle();
+    $(".initial-hiddencm").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable10").click(function() {
+    $(".initial-showingstr").toggle();
+    $(".initial-hiddenstr").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable11").click(function() {
+    $(".initial-showingboo").toggle();
+    $(".initial-hiddenboo").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable12").click(function() {
+    $(".initial-showingund").toggle();
+    $(".initial-hiddenund").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".clickable13").click(function() {
+    $(".initial-showingnan").toggle();
+    $(".initial-hiddennan").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".clickable14").click(function() {
+    $(".initial-showingesc").toggle();
+    $(".initial-hiddenesc").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".clickable15").click(function() {
+    $(".initial-showingale").toggle();
+    $(".initial-hiddenale").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".clickable16").click(function() {
+    $(".initial-showingcom").toggle();
+    $(".initial-hiddencom").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".clickable17").click(function() {
+    $(".initial-showingjq").toggle();
+    $(".initial-hiddenjq").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".clickable18").click(function() {
+    $(".initial-showingatt").toggle();
+    $(".initial-hiddenatt").toggle();
+  });
+});
